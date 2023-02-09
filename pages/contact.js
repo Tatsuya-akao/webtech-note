632a8f5175ed5f4a29541ddeeccc3866c81bc8cf
Tatsuya <a.tatsu0117@gmail.com>
2023-02-10 02:26:11 +0900

    hide contact until bugs get  fixed

--- pages/contact.js
+++ pages/contact.js
@@ -1,55 +1,55 @@
-import Layout from "@/components/layout/layout";
-import Container from "@/components/layout/container";
-import Router from "next/router";
-import Form from "@/components/layout/form";
-import s from "@/styles/pages/contact.module.scss";
-import { NextSeo } from "next-seo";
-import { siteTitle, domain } from "@/utils/seo";
+// import Layout from "@/components/layout/layout";
+// import Container from "@/components/layout/container";
+// import Router from "next/router";
+// import Form from "@/components/layout/form";
+// import s from "@/styles/pages/contact.module.scss";
+// import { NextSeo } from "next-seo";
+// import { siteTitle, domain } from "@/utils/seo";
 
-const Contact = () => {
-  return (
-    <>
-      <NextSeo
-        title={`Contact | ${siteTitle}`}
-        description="This is a contact page. You can send me messages about WebTech Note, or job offers."
-        openGraph={{
-          images: [
-            {
-              url: `${domain}/ogp.png`,
-              width: 1300,
-              height: 630,
-              alt: "WebTech Note",
-            },
-          ],
-        }}
-      />
-      <Layout>
-        <Container>
-          <main className={s.contact}>
-            <Container additionalClass={s.container}>
-              <h1 className={s.main_title}>Contact</h1>
-              <p className={s.lead_text}>
-                Feel free to contact me from the form below. I'll get back to
-                you within 3 days.
-                <br />
-                If you need a quick response or wanna have casual conversation,
-                please send me a DM on{" "}
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://twitter.com/a_tatsu1512"
-                >
-                  Twitter
-                </a>
-                .
-              </p>
-              <Form />
-            </Container>
-          </main>
-        </Container>
-      </Layout>
-    </>
-  );
-};
+// const Contact = () => {
+//   return (
+//     <>
+//       <NextSeo
+//         title={`Contact | ${siteTitle}`}
+//         description="This is a contact page. You can send me messages about WebTech Note, or job offers."
+//         openGraph={{
+//           images: [
+//             {
+//               url: `${domain}/ogp.png`,
+//               width: 1300,
+//               height: 630,
+//               alt: "WebTech Note",
+//             },
+//           ],
+//         }}
+//       />
+//       <Layout>
+//         <Container>
+//           <main className={s.contact}>
+//             <Container additionalClass={s.container}>
+//               <h1 className={s.main_title}>Contact</h1>
+//               <p className={s.lead_text}>
+//                 Feel free to contact me from the form below. I'll get back to
+//                 you within 3 days.
+//                 <br />
+//                 If you need a quick response or wanna have casual conversation,
+//                 please send me a DM on{" "}
+//                 <a
+//                   target="_blank"
+//                   rel="noreferrer"
+//                   href="https://twitter.com/a_tatsu1512"
+//                 >
+//                   Twitter
+//                 </a>
+//                 .
+//               </p>
+//               <Form />
+//             </Container>
+//           </main>
+//         </Container>
+//       </Layout>
+//     </>
+//   );
+// };
 
-export default Contact;
+// export default Contact;
